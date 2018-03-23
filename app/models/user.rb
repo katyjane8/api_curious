a4b5505b962a798a39c8f81389a45f008349e095
katyjane8 <katy.jane8@gmail.com>
2018-03-22 19:21:43 -0600

    user logic includes token authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth_info)
     user = where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.uid      = auth_info.uid
-      new_user.login    = auth_info.info.username
+      new_user.login    = auth_info.info.nickname
       new_user.token    = auth_info.credentials.token
     end
     verify_current_token(user, auth_info)
