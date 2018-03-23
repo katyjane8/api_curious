e9514d748dbbfab05d70311a72343b245ee8dddc
katyjane8 <katy.jane8@gmail.com>
2018-03-22 19:24:32 -0600

    service has methods that are called in the view

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,5 +1,5 @@
 class GithubService
-  attr_reader :user
+  attr_reader :user, :other_user
 
   def initialize(user)
     @user = user
@@ -12,30 +12,52 @@ class GithubService
     end
   end
 
-  def get_user_info(user)
+  def get_user_info
     response = conn(user.token).get "/user"
     parse_json(response)
   end
 
-  def get_starred_repos(user)
+  def get_starred_repos
     response = conn(user.token).get "/users/#{user.login}/starred"
     parse_json(response)
   end
 
-  def get_followers(user)
+  def get_followers
     response = conn(user.token).get "/users/#{user.login}/followers"
     parse_json(response)
   end
 
-  def get_following(user)
+  def get_following
     response = conn(user.token).get "/users/#{user.login}/following"
     parse_json(response)
   end
 
-  def get_repos(user)
+  def get_repos
     response = conn(user.token).get "/user/repos"
     parse_json(response)
   end
+
+  def get_events
+    response = conn(user.token).get "/users/#{user.login}/events"
+    events = parse_json(response)
+    push_events = events.select do |event|
+      event["type"] == "PushEvent"
+    end
+    push_events.map do |event|
+       { repo_name: event["repo"]["name"], message: event["payload"]["commits"].first["message"] }
+     end
+  end
+
+  # def get_others_events
+  #   response = conn(user.token).get "/users/#{other_user.login}/events"
+  #   events = parse_json(response)
+  #   push_events = events.select do |event|
+  #     event["type"] == "PushEvent"
+  #   end
+  #   push_events.map do |event|
+  #      { repo_name: event["repo"]["name"], message: event["payload"]["commits"].first["message"] }
+  #    end
+  # end
 
   private
 
